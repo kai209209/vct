63d7ecfc2f2d3c8ee746ddd642ff5d25be27984d
zky <361561797@qq.com>
2017-01-18 15:32:23 +0800

    好友申请通过之后创建好友关系数据

--- app/models/apply.rb
+++ app/models/apply.rb
@@ -8,4 +8,11 @@ class Apply < ApplicationRecord
 
   scope :apply_status, -> { where(apply_status: 0) }
 
+
+  def create_user_friends_relationship
+    FriendsRelationship.create(user: self.author, friend: self.receiver)
+    FriendsRelationship.create(user: self.receiver, friend: self.author)
+    # Conversation.create(author: self.author, receiver: self.receiver)
+  end
+
 end
